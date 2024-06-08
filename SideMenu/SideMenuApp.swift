235f71d88ca7ce403a868a28892e18597296bcf1
Jaime Lucea <jlucea@eservicios.indra.es>
2024-06-08 12:34:37 +0200

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Jaime Lucea on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
